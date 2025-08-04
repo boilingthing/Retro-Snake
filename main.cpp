e240da5d6d2d0e4eb8a6bdd80e88043426a5b55a
boilingthing <boilingthing@outlook.com>
2025-08-04 16:12:18 +0800

    Creating the gameloop

--- main.cpp
+++ main.cpp
@@ -3,41 +3,24 @@
 
 using namespace std;
 
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
 int main () {
 
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
-
-    cout << "Hello World" << endl;
-
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
+    cout << "Starting the game..." << endl;
+    InitWindow(750, 750, "Retro Snake");
     SetTargetFPS(60);
 
-    while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-        
+    while(!WindowShouldClose()){
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+
+        //Drawing
+        ClearBackground(green);
+
         EndDrawing();
     }
 
     CloseWindow();
+    return 0;
 }
